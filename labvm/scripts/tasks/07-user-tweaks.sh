64f4e9278948c45ab9632db2a8d6528049d85027
Stancu Florin <niflostancu@gmail.com>
2023-03-05 17:00:25 +0200

    tweaks: add user zsh + tmux configuration

--- labvm/scripts/tasks/07-user-tweaks.sh
+++ labvm/scripts/tasks/07-user-tweaks.sh
@@ -2,21 +2,29 @@
 # VM install initialization
 [[ "$INSIDE_INSTALL_SCRIPT" == "1" ]] || { echo "Direct calls not supported!">&2; exit 5; }
 
-# Copy configs
-#rsync -ai --chown="root:root" "$SRC/files/etc/" /etc/
 
 # copy user home configs
-_copy_home_config() {
-	# copy & enable bashrc:
-	cp -f "$SRC/files/home/bashrc" "$1"/.bashrc
-	chmod 755 "$1"/.bashrc
-	chown "$2:$2" "$1"/.bashrc
-	# copy tmux config
-	mkdir -p "$1"/.config/tmux/
-	rsync -ai --chown="$2:$2" "$SRC/files/home/tmux/" "$1/.config/tmux/"
-	ln -sf "$1/.config/tmux/tmux.conf" "$1/.tmux.conf"
-	chown "$2:$2" "$1/.tmux.conf"
+_install_home_config() {
+	local USER="$1"
+	local DEST="$2"
+	# bashrc:
+	install -m755 -o"$USER" -g"$USER" "$SRC/files/home/bashrc" "$DEST/.bashrc"
+
+	# tmux config (for user, only):
+	if [[ "$USER" == "student" ]]; then
+		rsync -ai --mkpath --chown="$USER:$USER" "$SRC/files/home/tmux/" "$DEST/.config/tmux/"
+		ln -sf "$DEST/.config/tmux/tmux.conf" "$DEST/.tmux.conf"
+		chown "$USER:$USER" "$DEST/.tmux.conf"
+	fi
+
+	# zsh config:
+	rsync -ai --mkpath --chown="$USER:$USER" "$SRC/files/home/zsh/" "$DEST/.config/zsh/"
+	ln -sf "$DEST/.config/zsh/zshrc" "$DEST/.zshrc"
+	chown "$USER:$USER" "$DEST/.zshrc"
+	chsh -s /usr/bin/zsh "$USER"
+	# run zsh for user to install plugins
+	su -c 'zsh -c "source ~/.zshrc; exit 0"' - "$USER"
 }
-_copy_home_config /root root
-_copy_home_config /home/student student
+_install_home_config root /root
+_install_home_config student /home/student
 
