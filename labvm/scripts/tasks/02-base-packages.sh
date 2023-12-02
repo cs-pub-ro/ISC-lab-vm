3ae3f94fc5010e8341984e74ec060b9169423e47
Stancu Florin <niflostancu@gmail.com>
2023-12-02 13:12:46 +0200

    scripts: docker: update install commands
    Based on newest official instructions for Ubuntu.

--- labvm/scripts/tasks/02-base-packages.sh
+++ labvm/scripts/tasks/02-base-packages.sh
@@ -3,8 +3,8 @@
 [[ "$INSIDE_INSTALL_SCRIPT" == "1" ]] || { echo "Direct calls not supported!">&2; exit 5; }
 
 apt-get install --no-install-recommends -y \
-	apt-transport-https ca-certificates curl wget software-properties-common \
-	git unrar unzip zsh vim neovim # parallel
+	apt-transport-https software-properties-common ca-certificates gnupg \
+	curl wget git unrar unzip zsh vim neovim
 
 if [[ -n "$FULL_UPGRADE" ]]; then
 	apt-get dist-upgrade -y
--- labvm/scripts/tasks/21-docker.sh
+++ labvm/scripts/tasks/21-docker.sh
@@ -4,11 +4,20 @@
 
 # container tools
 apt-get install -y bridge-utils
+
 # official docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+install -m 0755 -d /etc/apt/keyrings
+[[ -f "/etc/apt/keyrings/docker.gpg" ]] || \
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+ls -lh /etc/apt/keyrings/
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-compose
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # docker without sudo
 usermod -aG docker student || true
 
