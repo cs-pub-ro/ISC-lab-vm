72ec04499739cd92dfc665b0da313ead69bbe867
Stancu Florin <niflostancu@gmail.com>
2025-11-13 14:25:15 +0200

    network: make hostname change idempotent

--- labvm/scripts/full-snippets.d/12-network.sh
+++ labvm/scripts/full-snippets.d/12-network.sh
@@ -2,8 +2,8 @@
 [[ -n "$__INSIDE_VM_RUNNER" ]] || { echo "Only call within VM runner!" >&2; return 1; }
 # VM network configuration
 
-# Change hostname to host
-if [[ "$(hostname)" != "host" ]]; then
+# Change hostname to isc-vm
+if [[ "$(hostname)" != "isc-vm" ]]; then
 	hostnamectl set-hostname isc-vm
 	sed -i "s/^127.0.1.1\s.*/127.0.1.1       isc-vm/g"  /etc/hosts
 fi
