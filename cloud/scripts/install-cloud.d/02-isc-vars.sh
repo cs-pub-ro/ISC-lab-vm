8247e0fe0086df041387e650acf70f11b372adbd
Stancu Florin <niflostancu@gmail.com>
2025-12-04 12:06:28 +0200

    cloud: migrate to framework's VM_CONSOLE_SECURITY feature

---
+++ cloud/scripts/install-cloud.d/02-isc-vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+[[ -n "$__INSIDE_VM_RUNNER" ]] || { echo "Only call within VM runner!" >&2; return 1; }
+## ISC cloud VM overrides
+
+# enable legacy SSH algs
+VM_SSH_LEGACY_ALGS=1
+# special setting, disable this
+VM_PASSWORD=
+# Disable SSH password auth (only enabled for admin via special config)
+VM_SSH_PASSWORD_AUTH=0
+
+# Enable console security scripts (disable password-based login, ctrl-alt-del etc.)
+VM_CONSOLE_SECURITY=1
+
